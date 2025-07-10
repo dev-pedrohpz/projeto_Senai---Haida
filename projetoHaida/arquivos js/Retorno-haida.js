574365320b7ac5fb44d0abefb9c27d1fb71157ca
dev-pedrohpz <pedrorodriguespazdacosta@gmail.com>
2025-07-10 08:11:55 -0300

    Add files via upload

---
+++ projetoHaida/arquivos js/Retorno-haida.js
@@ -0,0 +1,4 @@
+let botaoRetornar = document.getElementById('botao-retornar')
+botaoRetornar.addEventListener('click', function(){
+    window.location.href = '../index.html';
+})
---
+++ projetoHaida/arquivos js/Trilhas-haida.js
@@ -0,0 +1,63 @@
+const botaoPY = document.getElementById("python1")
+const botaoJS = document.getElementById("botaoJS1")
+const botaoCC = document.getElementById("c++1")
+const botaoCsharp = document.getElementById("botaoCsharp1")
+const botaoRUBY = document.getElementById("ruby1")
+const botaoPHP = document.getElementById("php1")
+const botaoHTML = document.getElementById("html1")
+const botaoJAVA = document.getElementById("java1")
+const botaoCSS = document.getElementById("css1")
+        
+botaoJS.addEventListener('click', function(){
+
+    window.location.href = 'paginas/javascript.html' ;
+
+})
+
+botaoCsharp.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/csharp.html'
+
+})
+
+botaoPY.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/pyton.html'
+
+})
+
+botaoCC.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/cmais.html'
+
+})
+
+botaoRUBY.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/ruby.html'
+
+})
+
+botaoPHP.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/php.html'
+
+})
+
+botaoHTML.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/ht.html'
+
+})
+
+botaoJAVA.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/java.html'
+
+})
+
+botaoCSS.addEventListener('click', function(){  
+
+    window.location.href = 'paginas/css.html'
+
+})
